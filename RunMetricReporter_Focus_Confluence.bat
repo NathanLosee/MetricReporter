38bae3fb918b01d782c998257516c2468e7e9d55
Nathan Losee <Nathan_Losee@papajohns.com>
2020-12-09 09:20:13 -0500

    Added curbside metrics.

--- RunMetricReporter_Focus_Confluence.bat
+++ RunMetricReporter_Focus_Confluence.bat
@@ -1,1 +1,1 @@
-java -cp MetricReporter.jar;./lib/* com.MetricReporter.MetricReporter "Focus" "11/20/2020-13:45" "60" "Confluence" "Test 197 | Release 20.18 | Build 2 | Focus 3.72.0-04 | Baseline 3 Results" "true"
+java -cp MetricReporter.jar;./lib/* com.MetricReporter.MetricReporter "Focus" "12/09/2020-10:30" "60" "Confluence" "Test 204 | Release 20.20 | Build 1 | Focus 3.74.0-03 | Baseline Test 1" "true"
